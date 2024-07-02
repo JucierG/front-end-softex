bbf41170e47e50cf08de72009391c35849edd911
JucierG <jucier.gregorio05@gmail.com>
2024-07-02 17:33:34 -0300

    array, boolean, integer-e-float, objeto, operadores-logicos, string

---
+++ atividade-02-tipos-de-dados/operadores-logicos.ts
@@ -0,0 +1,36 @@
+//Questão 1
+console.log(2 == 2)
+
+//Questão 2
+console.log(2 != 2)
+
+//Questão 3
+console.log(15 > 5)
+
+//Questão 4
+console.log(15 < 5)
+
+//Questão 5
+console.log(10 >= 10)
+
+//Questão 6
+console.log(10 <= 5)
+
+//Questão 7
+console.log(10 >= 10 && 10 < 5)
+
+//Questão 8
+console.log(10 >= 10 || 10 < 5)
+
+//Questão 9
+console.log(30 != 30)
+
+//Questão 10
+console.log(20 >= 20 && 20 < 5)
+
+//Questão 13
+let palavra: string = ""
+console.log(palavra.length == 0)
+
+//Questão 14
+console.log(typeof(palavra  == boolean))
