35d2a0ad17cdf26034494d2b1464022ba4a650dc
JucierG <jucier.gregorio05@gmail.com>
2024-07-02 17:22:39 -0300

    array, boolean, integer-e-float, objeto, operadores-logicos, string

---
+++ atividade-02-tipos-de-dados/string.ts
@@ -0,0 +1,20 @@
+// Questão 1
+let nome : string = "Jucier"
+console.log(nome)
+
+//Questão 2
+let frase1 : string = "Rosas são vermelhas"
+console.log(frase1.length)
+
+//Questão 3
+let palavra : string = "Planalto"
+console.log(palavra.charAt(0))
+
+//Questão 4
+let frase2 : string = "Quando o segundo sol chegar"
+console.log(frase2.toUpperCase())
+
+//Questão 5
+let endereco : string = "Shopping Guararapes"
+endereco = "Curado"
+console.log(endereco)
